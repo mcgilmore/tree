8e507063de13ffa75585a9ba4ac3939017f19a2d
Michael G <mg393@users.noreply.github.com>
2023-05-19 17:22:58 +0200

    Kinda works

--- tree.py
+++ tree.py
@@ -2,29 +2,25 @@ import sys
 import subprocess
 import csv
 import re
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QPushButton, QTextEdit, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QPushButton, QLineEdit, QWidget, QFileDialog
 
 class OrthoDBGUI(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("OrthoDB GUI")
-        self.setGeometry(200, 200, 400, 400)
+        self.setWindowTitle("Tree")
 
         layout = QVBoxLayout()
 
         label = QLabel("Enter OrthoDB Group ID:")
         layout.addWidget(label)
 
-        self.text_edit = QTextEdit()
+        self.text_edit = QLineEdit()
         layout.addWidget(self.text_edit)
 
-        button = QPushButton("Retrieve Organism Names")
+        button = QPushButton("Create iTOL Dataset")
         button.clicked.connect(self.retrieve_organism_names)
         layout.addWidget(button)
-
-        self.result_text_edit = QTextEdit()
-        layout.addWidget(self.result_text_edit)
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -39,7 +35,7 @@ class OrthoDBGUI(QMainWindow):
             return None
 
     def retrieve_organism_names(self):
-        group_id = self.text_edit.toPlainText()
+        group_id = self.text_edit.text()
 
         curl_command = f'curl -X GET "https://data.orthodb.org/current/tab?id={group_id}"'
         response = self.run_curl_command(curl_command)
@@ -56,10 +52,25 @@ class OrthoDBGUI(QMainWindow):
             for row in reader:
                 if len(row) >= 5:
                     organism_name = row[4]  # Column 5 (0-based index)
+                    organism_name = organism_name.replace(" ", "_")
                     organism_names.append(organism_name)
-                    print(row)
 
-            self.result_text_edit.setPlainText('\n'.join(organism_names))
+            file_dialog = QFileDialog()
+            options = QFileDialog.Options()
+            file_path, _ = file_dialog.getSaveFileName(self, "Save iTOL Dataset", "", "Text Files (*.txt)", options=options)
+
+            if file_path:
+                with open(file_path, "w") as file:
+                    file.write("DATASET_GRADIENT\n")
+                    file.write("#Dataset created using tree.py\n\n")
+                    file.write("SEPARATOR TAB\n")
+                    file.write("DATASET_LABEL\t{}\n".format(group_id))
+                    file.write("COLOR\t#00ff00\n")
+                    file.write("COLOR_MIN\t#00ff00\n")
+                    file.write("COLOR_MAX\t#0000ff\n\n")
+                    file.write("DATA\n")
+                    for organism_name in organism_names:
+                        file.write(f"{organism_name}\t1\n")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
